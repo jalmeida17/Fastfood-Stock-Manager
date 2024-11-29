01087228710e2d1a91c5ae26b6901d5efd71473b
Joao Almeida <jalmeidasantos@clauger.fr>
2024-11-29 11:30:29 +0100

    [ADD] Default API and Angular interface

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LEARNING-.NET-API-ANGULAR
+
+Repo d'apprentisage ou je fais une API .NET qui se connecte a une interface Angular
---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace LEARNING_.NET_API_ANGULAR;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
