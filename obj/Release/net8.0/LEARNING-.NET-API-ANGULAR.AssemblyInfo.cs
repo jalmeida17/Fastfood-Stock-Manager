1be3f527443ee1dbdfce902a0ba3cb93f7b735bb
Joao Almeida <jalmeidasantos@clauger.fr>
2024-11-29 14:17:09 +0100

    [DELETE] Had to delete front due to an error (will make it again)

--- obj/Release/net8.0/LEARNING-.NET-API-ANGULAR.AssemblyInfo.cs
+++ obj/Release/net8.0/LEARNING-.NET-API-ANGULAR.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LEARNING-.NET-API-ANGULAR")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ecbcc70efae7693b3fde8590485b11cef8357f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LEARNING-.NET-API-ANGULAR")]
